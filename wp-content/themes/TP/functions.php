f20fc46e8f017c32f6d5eec95531fadf64328543
Valentin Milite <vmilite02@gmail.com>
2022-12-09 11:54:22 +0100

    actualisation

--- wp-content/themes/TP/functions.php
+++ wp-content/themes/TP/functions.php
@@ -1,4 +1,12 @@
 <?php
+<<<<<<< Updated upstream
 add_action('after_setup_theme', function() {
     add_theme_support( 'title-tag' );
-});
+});=======
+define('DS', DIRECTORY_SEPARATOR);
+require dirname(__FILE__) . DS . 'inc' . DS . 'assets.php';
+
+
+
+?>
+>>>>>>> Stashed changes
