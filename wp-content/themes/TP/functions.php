11e34c7301866efa43119e61725dafe08d0ceea3
Valentin Milite <vmilite02@gmail.com>
2022-12-09 11:57:53 +0100

    renvoie

--- wp-content/themes/TP/functions.php
+++ wp-content/themes/TP/functions.php
@@ -1,13 +1,11 @@
 <?php
-<<<<<<< Updated upstream
 add_action('after_setup_theme', function() {
     add_theme_support( 'title-tag' );
 });
-=======
+
 define('DS', DIRECTORY_SEPARATOR);
 require dirname(__FILE__) . DS . 'inc' . DS . 'assets.php';
 
 
 
 ?>
->>>>>>> Stashed changes
--- wp-content/themes/TP/partials/head.php
+++ wp-content/themes/TP/partials/head.php
@@ -1,17 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<<<<<<< Updated upstream
     <title><?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
     <?php wp_head() ?>
 </head>
 <body <?php body_class();?>>
-=======
-    <?php wp_head(); ?>
-
-</head>
-<body>
-    <?php wp_body_open(); ?>
-    
-<h1>COUCOU</h1>
->>>>>>> Stashed changes
