2f177849f68e3e950515425793f8cac85a077581
Valentin Milite <vmilite02@gmail.com>
2022-12-09 10:57:52 +0100

    initialisation theme

---
+++ wp-content/themes/TP/index.php
@@ -0,0 +1,3 @@
+<?php get_template_part('partials/head.php'); ?>
+
+<?php get_template_part('partials/foot'); ?>
---
+++ wp-content/themes/TP/partials/head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    coucou
+    
+
+</head>
+<?php wp_head(); ?>
+<body>
+    <?php wp_body_open(); ?>
+    
+    test
