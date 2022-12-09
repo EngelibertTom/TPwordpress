6aa81d8b7e4b8534fb03b3eb8612604d751d08cd
Valentin Milite <vmilite02@gmail.com>
2022-12-09 11:55:16 +0100

    actualisation

--- wp-content/themes/TP/functions.php
+++ wp-content/themes/TP/functions.php
@@ -3,6 +3,8 @@
 add_action('after_setup_theme', function() {
     add_theme_support( 'title-tag' );
 });=======
+});
+=======
 define('DS', DIRECTORY_SEPARATOR);
 require dirname(__FILE__) . DS . 'inc' . DS . 'assets.php';
 
