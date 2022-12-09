8db3458f3a3d7b39ce8915ec57a853c813f35019
EngelibertTom <82039753+EngelibertTom@users.noreply.github.com>
2022-12-09 14:53:10 +0100

    offres

---
+++ wp-content/themes/TP/inc/post_types.php
@@ -0,0 +1,12 @@
+<?php
+add_action('init', function () {
+    register_post_type('offres', [
+        'labels' => [
+            'singular' => 'Offres',
+            'name' => 'Offres',
+        ],
+        'public' => true,
+        'has_archive' => true,
+        'supports' => ['title', 'editor', 'thumbnail'],
+    ]);
+});
